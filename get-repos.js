0a7abe42b32da0f4b4d077e48d3b81a69473ea14
Christian Pujol <pujolchr@gmail.com>
2017-09-27 02:01:16 +0200

    function to get repos info

---
+++ get-repos.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env nodejs
+const fetch = require('node-fetch');
+
+const apiUrl = 'api.github.com/users/pujolchr/repos';
+const tok = 'e104bc35784bd30d54d41e8eabe8818c8d88fb86';
+const repository = [];
+const getRepos = url => fetch(url) // check for error?
+  .then(data => data.json())
+  .then((data) => {
+    if (data.message) {
+      console.log(data);
+      return;
+    }
+    data.forEach((elmt) => {
+      const repo = {};
+      repo.name = elmt.name;
+      repo.description = elmt.description;
+      repo.html_url = elmt.html_url;
+      repo.url = elmt.url;
+
+      if (elmt.forks_count) repo.forks_count = elmt.forks_count;
+      repository.push(repo);
+    });
+  })
+  .then(() => Promise.all(repository.map((r) => {
+    const repo = r;
+    const rUrl = `https://pujolchr:${tok}@api.github.com/repos/pujolchr/${repo.name}`;
+    return fetch(rUrl)
+      .then(repoData => repoData.json())
+      .then((repoData) => {
+        repo.homepage = repoData.homepage;
+        repo.languages_url = repoData.languages_url;
+      });
+  })))
+  .then(() => Promise.all(repository.map((r) => {
+    const repo = r;
+    const rUrl = `https://pujolchr:${tok}@api.github.com/repos/pujolchr/${repo.name}/languages`;
+    return fetch(rUrl)
+      .then(lang => lang.json())
+      .then((languages) => {
+        repo.languages = Object.getOwnPropertyNames(languages);
+      });
+  })))
+  .then(() => repository);
+
+
+getRepos(`https://pujolchr:${tok}@${apiUrl}`).then(r => console.log(JSON.stringify(r)));
+module.exports = getRepos;
